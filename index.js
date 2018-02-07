1d7c56f08f7f7f564bbe6a12e5b00fbdcfaecbe1
Non3 <samGameDesign@gmail.com>
2018-02-07 18:27:05 +0700

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('io').listen(server);
 
 app.set('port', process.env.PORT || 8080);
 app.get('/', function(req, res) {
