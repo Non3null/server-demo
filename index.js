95fd156764f761e585c5c48ade10330763a18898
Non3 <samGameDesign@gmail.com>
2018-02-07 18:13:22 +0700

    first commit

--- index.js
+++ index.js
@@ -5,13 +5,6 @@ var io = require('io').listen(server);
 
 app.set('port', process.env.PORT || 8080);
 app.get('/', function(req, res) {
-    console.log("client connecting")
+    console.log("client connecting");
     res.send("Hello express");
-});
-
-var room_users = [];
-var display_user;
-
-io.on('connection', function (socket) {
-    
 });
